e8b8b80ef593c396e62e8b328078b49194945da2
Ludovic Dubois <ldubois@prettyobjects.com>
2017-04-17 22:17:58 -0400

    ThreeCasesTests: Missing first and third cases added.

--- SmartTestsAnalyzer/SmartTestsAnalyzer.Test/ThreeCasesTests.cs
+++ SmartTestsAnalyzer/SmartTestsAnalyzer.Test/ThreeCasesTests.cs
@@ -51,6 +51,44 @@ namespace TestingProject
         }
 
 
+        [Test]
+        public void MissingFirstAndThirdCases()
+        {
+            var test = @"
+using System;
+using NUnit.Framework;
+using SmartTests.Criterias;
+using static SmartTests.SmartTest;
+
+namespace TestingProject
+{
+    [TestFixture]
+    public class MyTestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var result = RunTest( Case( MinIncluded.IsMin ), () => Math.Sqrt(0) );
+
+            Assert.That( result, Is.EqualTo(0) );
+        }
+    }
+}";
+            var expected = new DiagnosticResult
+                           {
+                               Id = "SmartTestsAnalyzer_MissingCases",
+                               Message = "Tests for 'Math.Sqrt' has some missing Test Cases: MinIncluded.IsBelowMin and MinIncluded.IsAboveMin",
+                               Severity = DiagnosticSeverity.Warning,
+                               Locations = new[]
+                                           {
+                                               new DiagnosticResultLocation( "Test0.cs", 15, 41 )
+                                           }
+                           };
+
+            VerifyCSharpDiagnostic( test, expected );
+        }
+
+
         protected override CodeFixProvider GetCSharpCodeFixProvider() => new SmartTestsAnalyzerCodeFixProvider();
 
 
