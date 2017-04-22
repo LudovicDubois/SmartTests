143875976a210a4639de7965f7f78c6edaeecd32
Ludovic Dubois <ldubois@prettyobjects.com>
2017-04-21 20:48:05 -0400

    ThreeCasesTests: Missing second and third Cases Test

---
+++ TestingProject/ThreeCasesTests/MissingSecondAndThirdCasesTest .cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using SmartTests.Criterias;
+
+using static SmartTests.SmartTest;
+
+
+
+namespace TestingProject.ThreeCasesTests
+{
+    [TestFixture]
+    public class MissingSecondAndThirdCasesTest
+    {
+        // This method is here to be independent of the other tests
+        private static double Sqrt( double value ) => Math.Sqrt( value );
+
+
+        [Test]
+        public void TestMethod()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>( () => RunTest( Case( MinIncluded.IsBelowMin ), () => Sqrt( 4 ) ) );
+        }
+    }
+}
