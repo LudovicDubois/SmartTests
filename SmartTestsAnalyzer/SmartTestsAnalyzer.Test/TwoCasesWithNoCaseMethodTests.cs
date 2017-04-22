962cd4d7bd49902417af8ba5b5176169b3d94f55
Ludovic Dubois <ldubois@prettyobjects.com>
2017-04-21 21:06:03 -0400

    No Case method call.

---
+++ SmartTestsAnalyzer/SmartTestsAnalyzer.Test/TwoCasesWithNoCaseMethodTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+
+
+namespace SmartTestsAnalyzer.Test
+{
+    [TestFixture]
+    public class TwoCasesWithNoCaseMethodTests: CodeFixVerifier
+    {
+        [Test]
+        public void MissingNoCase()
+        {
+            var test = @"
+using System;
+using NUnit.Framework;
+using SmartTests.Criterias;
+using static SmartTests.SmartTest;
+
+namespace TestingProject
+{
+    [TestFixture]
+    public class MyTestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var result = RunTest( ValidValue.Valid, () => Math.Sqrt(4) );
+
+            Assert.That( result, Is.EqualTo(2) );
+        }
+
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.Throws<IndexOutOfRangeException>( () => RunTest( ValidValue.Invalid, () => Math.Sqrt(-4) ) );
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic( test );
+        }
+
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new SmartTestsAnalyzerCodeFixProvider();
+
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new SmartTestsAnalyzerAnalyzer();
+    }
+}
---
+++ TestingProject/TwoCasesTests/NoCaseMethodTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using SmartTests.Criterias;
+
+using static SmartTests.SmartTest;
+
+
+
+namespace TestingProject.TwoCasesTests
+{
+    [TestFixture]
+    public class NoCaseMethodTest
+    {
+        // This method is here to be independent of the other tests
+        private static double Sqrt( double value ) => Math.Sqrt( value );
+
+
+        [Test]
+        public void TestMethod()
+        {
+            var result = RunTest( ValidValue.Valid, () => Sqrt( 4 ) );
+
+            Assert.That( result, Is.EqualTo( 2 ) );
+        }
+
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.Throws<IndexOutOfRangeException>( () => RunTest( ValidValue.Invalid, () => Sqrt( -4 ) ) );
+        }
+    }
+}
