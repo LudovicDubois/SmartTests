10b029050973436450b413b066e0255fac2c0223
Ludovic Dubois <ldubois@prettyobjects.com>
2017-05-17 21:03:58 -0400

    Multiple Criteria Tests with & and |

---
+++ TestingProject/MultipleCriteriasTests/AndOrMissing1CaseTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+using SmartTests.Criterias;
+
+using static SmartTests.SmartTest;
+
+
+
+namespace TestingProject.MultipleCriteriasTests
+{
+    [TestFixture]
+    public class AndOrMissing1CaseTest
+    {
+        public class MyClass
+        {
+            public MyClass( int property )
+            {
+                Property = property;
+            }
+
+
+            public int Property { get; set; }
+        }
+
+
+        [Test]
+        public void MyTest()
+        {
+            var mc = new MyClass( 10 );
+            Assert.That( mc.Property, Is.EqualTo( 10 ) );
+
+            var result = RunTest( ValidValue.Valid & NotifyPropertyChanged.HasNoSubscriber | ValidValue.Valid & NotifyPropertyChanged.HasSubscriberSameValue | ValidValue.Valid & NotifyPropertyChanged.HasSubscriberOtherValue,
+                                  Assign( () => mc.Property, 11 ) );
+
+            Assert.That( result, Is.EqualTo( 11 ) );
+            Assert.That( mc.Property, Is.EqualTo( 11 ) );
+        }
+
+
+        /*
+        var expected = new DiagnosticResult
+                        {
+                            Id = "SmartTestsAnalyzer_MissingCases",
+                            Message = "Tests for 'TestingProject.MyTestClass.MyClass.Property [set]' has some missing Test Cases: ValidValue.Invalid",
+                            Severity = DiagnosticSeverity.Warning,
+                            Locations = new[]
+                                        {
+                                            new DiagnosticResultLocation( "Test0.cs", 27, 35 )
+                                        }
+                        };
+         */
+    }
+}
---
+++ TestingProject/MultipleCriteriasTests/AndOrMissingNoCaseTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+using SmartTests.Criterias;
+
+using static SmartTests.SmartTest;
+
+
+
+namespace TestingProject.MultipleCriteriasTests
+{
+    [TestFixture]
+    public class AndOrMissingNoCaseTest
+    {
+        public class MyClass
+        {
+            public MyClass( int property )
+            {
+                Property = property;
+            }
+
+
+            public int Property { get; set; }
+        }
+
+
+        [Test]
+        public void MyTest()
+        {
+            var mc = new MyClass( 10 );
+            Assert.That( mc.Property, Is.EqualTo( 10 ) );
+
+            var result = RunTest( ValidValue.Valid & NotifyPropertyChanged.HasNoSubscriber | ValidValue.Valid & NotifyPropertyChanged.HasSubscriberSameValue | ValidValue.Valid & NotifyPropertyChanged.HasSubscriberOtherValue | ValidValue.Invalid,
+                                  Assign( () => mc.Property, 11 ) );
+
+            Assert.That( result, Is.EqualTo( 11 ) );
+            Assert.That( mc.Property, Is.EqualTo( 11 ) );
+        }
+
+
+        // No error!
+    }
+}
