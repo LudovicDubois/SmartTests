482994cd7bf6dd0e9ae7d0d180236e7d980e44e9
Ludovic Dubois <ldubois@prettyobjects.com>
2017-09-16 17:24:46 -0400

    Nuget Packages.

--- SmartTestsAnalyzer/SmartTestsAnalyzer/Properties/AssemblyInfo.cs
+++ SmartTestsAnalyzer/SmartTestsAnalyzer/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SmartTestsAnalyzer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An analyzer to find missing tests for tests written with SmartTests")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pretty Objects Computers inc.")]
 [assembly: AssemblyProduct("SmartTestsAnalyzer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
