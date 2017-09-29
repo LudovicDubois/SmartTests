8631f0f808de2fd73d89ff097e5216486de6efb7
Ludovic Dubois <ldubois@prettyobjects.com>
2017-09-28 20:59:17 -0400

    Assertion XML documentation.

--- SmartTests/Assertion.cs
+++ SmartTests/Assertion.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
-
-
-namespace SmartTests
+﻿namespace SmartTests
 {
+    /// <summary>
+    ///     Base class of all Smart Assertions
+    /// </summary>
     public abstract class Assertion
     {
+        /// <summary>
+        ///     The method called before the Act in a test.
+        /// </summary>
+        /// <param name="act">The Act of your test</param>
+        /// <seealso cref="O:SmartTests.SmartTest.RunTest" />
         public abstract void BeforeAct( ActBase act );
+
+
+        /// <summary>
+        ///     The method called after the Act in a test.
+        /// </summary>
+        /// <param name="act">The Act of your test</param>
+        /// <seealso cref="O:SmartTests.SmartTest.RunTest" />
         public abstract void AfterAct( ActBase act );
     }
 }
