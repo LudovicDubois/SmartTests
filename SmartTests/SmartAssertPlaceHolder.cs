4de48620cb950f4e97eb5625844a787e2aa7bd3b
Ludovic Dubois <ldubois@prettyobjects.com>
2017-09-29 21:02:56 -0400

    SmartAssertPlaceHolder XML documentation added.

--- SmartTests/SmartAssertPlaceHolder.cs
+++ SmartTests/SmartAssertPlaceHolder.cs
@@ -1,5 +1,16 @@
-﻿namespace SmartTests
+﻿using JetBrains.Annotations;
+
+
+
+namespace SmartTests
 {
+    /// <summary>
+    ///     The place holder of any Smart Assertion extension method.
+    /// </summary>
+    /// <remarks>
+    ///     It does not do nothing. It is just a place holder so that it is very easy to add Smart Tests methods.
+    /// </remarks>
+    [PublicAPI]
     public class SmartAssertPlaceHolder
     { }
 }
