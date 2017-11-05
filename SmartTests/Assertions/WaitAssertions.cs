f0b9730f425ab761700b35aa75bbc8dc0a10acd0
Ludovic Dubois <ldubois@prettyobjects.com>
2017-11-05 10:43:59 -0500

    WaitAssertions added.

---
+++ SmartTests/Assertions/WaitAssertions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+
+using JetBrains.Annotations;
+
+
+
+namespace SmartTests.Assertions
+{
+    /// <summary>
+    ///     Helper class to test asynchronous tests.
+    /// </summary>
+    /// <seealso cref="SmartTest.SmartAssert" />
+    /// <seealso cref="SmartTest" />
+    [PublicAPI]
+    public static class WaitAssertions
+    {
+        /// <summary>
+        ///     Creates an <see cref="Assertion" /> that ensure an asynchronous call from the Act is done before continuing Smart
+        ///     Assertions validations.
+        /// </summary>
+        /// <param name="_">The dummy place holder for all Smart Assertions.</param>
+        /// <param name="evt">The <see cref="WaitHandle" /> to wait for before continuing Smart Assertions validations.</param>
+        /// <param name="timeout">The maximum milliseconds to wait for the handle to be set.</param>
+        /// <returns>The newly created <see cref="Assertion" />.</returns>
+        /// <exception cref="SmartTestException">
+        ///     If the <paramref name="evt" /> is not set within <paramref name="timeout" /> milliseconds.
+        /// </exception>
+        /// <example>
+        ///     <para> In this example, the Smart Assertion verifies that the <c>MyMethod</c> call launches a callback within 1s.</para>
+        ///     <code>
+        /// [Test]
+        /// public void MyMethodTest()
+        /// {
+        ///     var mc = new MyClass();
+        ///     var handle = new ManualResetEvent( false );
+        /// 
+        ///     RunTest( ValidValue.IsValid,
+        ///              () => mc.MyMethod( () => handle.set() ),
+        ///              SmartAssert.Wait( handle, 1000 ) );
+        /// }</code>
+        /// </example>
+        public static Assertion Wait( this SmartAssertPlaceHolder _, WaitHandle evt, double timeout ) => new WaitAssertion( evt, TimeSpan.FromMilliseconds( timeout ) );
+
+
+        /// <summary>
+        ///     Creates an <see cref="Assertion" /> that ensure an asynchronous call from the Act is done before continuing Smart
+        ///     Assertions validations.
+        /// </summary>
+        /// <param name="_">The dummy place holder for all Smart Assertions.</param>
+        /// <param name="evt">The <see cref="WaitHandle" /> to wait for before continuing Smart Assertions validations.</param>
+        /// <param name="timeout">The maximum milliseconds to wait for the handle to be set.</param>
+        /// <returns>The newly created <see cref="Assertion" />.</returns>
+        /// <exception cref="SmartTestException">
+        ///     If the <paramref name="evt" /> is not set within <paramref name="timeout" />.
+        /// </exception>
+        /// <example>
+        ///     <para> In this example, the Smart Assertion verifies that the <c>MyMethod</c> call launches a callback within 1s.</para>
+        ///     <code>
+        /// [Test]
+        /// public void MyMethodTest()
+        /// {
+        ///     var mc = new MyClass();
+        ///     var handle = new ManualResetEvent( false );
+        /// 
+        ///     RunTest( ValidValue.IsValid,
+        ///              () => mc.MyMethod( () => handle.set() ),
+        ///              SmartAssert.Wait( handle, TimeSpan.FromSeconds( 1 ) ) );
+        /// }</code>
+        /// </example>
+        public static Assertion Wait( this SmartAssertPlaceHolder _, WaitHandle evt, TimeSpan timeout ) => new WaitAssertion( evt, timeout );
+
+
+        private class WaitAssertion: Assertion
+        {
+            public WaitAssertion( WaitHandle evt, TimeSpan fromMilliseconds )
+            {
+                _Event = evt;
+                _FromMilliseconds = fromMilliseconds;
+            }
+
+
+            private readonly WaitHandle _Event;
+            private readonly TimeSpan _FromMilliseconds;
+
+
+            public override void BeforeAct( ActBase act )
+            { }
+
+
+            public override void AfterAct( ActBase act )
+            {
+                if( !_Event.WaitOne( _FromMilliseconds ) )
+                    throw new SmartTestException( Resource.TimeoutReached, _FromMilliseconds.TotalMilliseconds );
+            }
+        }
+    }
+}
---
+++ SmartTestsAnalyzer.Runtime.Test/WaitTests/WaitTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using SmartTests;
+using SmartTests.Assertions;
+using SmartTests.Criterias;
+
+using static SmartTests.SmartTest;
+
+
+
+namespace SmartTestsAnalyzer.Runtime.Test.WaitTests
+{
+    [TestFixture]
+    public class WaitTests
+    {
+        public class MyClass
+        {
+            public MyClass( int wait )
+            {
+                _Wait = wait;
+            }
+
+
+            private readonly int _Wait;
+
+            public bool Done { get; private set; }
+
+
+            public void Method( Action action ) => Task.Factory.StartNew( () =>
+                                                                          {
+                                                                              Thread.Sleep( _Wait );
+                                                                              action();
+                                                                              Done = true;
+                                                                          } );
+        }
+
+
+        [Test]
+        public void OK()
+        {
+            var handle = new AutoResetEvent( false );
+            var mc = new MyClass( 200 );
+
+            RunTest( AnyValue.IsValid,
+                     () => mc.Method( () => handle.Set() ),
+                     SmartAssert.Within( 100 ),
+                     SmartAssert.Wait( handle, 1000 ) );
+
+            Assert.IsTrue( mc.Done );
+        }
+
+
+        [Test]
+        public void Timeout()
+        {
+            var exception = Assert.Catch<SmartTestException>( () =>
+                                                              {
+                                                                  var handle = new AutoResetEvent( false );
+                                                                  var mc = new MyClass( 200 );
+
+                                                                  RunTest( AnyValue.IsValid,
+                                                                           () => mc.Method( () => handle.Set() ),
+                                                                           SmartAssert.Within( 100 ),
+                                                                           SmartAssert.Wait( handle, 150 ) );
+
+                                                                  Assert.IsTrue( mc.Done );
+                                                              } );
+
+            Assert.AreEqual( "Timeout reached (150ms)", exception.Message );
+        }
+    }
+}
