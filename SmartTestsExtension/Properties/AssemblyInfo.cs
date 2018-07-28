676f1d81804a94ee7772d471fa797e46f3930531
Ludovic Dubois <ldubois@prettyobjects.com>
2018-07-28 15:09:15 -0400

    v1.4.0

--- SmartTestsExtension/Properties/AssemblyInfo.cs
+++ SmartTestsExtension/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SmartTestsExtension")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An extension to display current and missing tests written with SmartTests and SmartTestsAnalyzer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pretty Obnjects Computers inc.")]
 [assembly: AssemblyProduct("SmartTestsExtension")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
